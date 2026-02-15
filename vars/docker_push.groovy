da63188e5b7e5faa13b85f174bf3127b39821a3f
Adnan Sharief <98080486+adnan-sharief@users.noreply.github.com>
2026-02-15 18:52:31 +0530

    Refactor docker_push function to use credentials

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,4 +1,4 @@
-def call(Map config = [:]) {
+/*def call(Map config = [:]) {
     def imageName = config.imageName ?: error("Image name is required")
     def imageTag = config.imageTag ?: 'latest'
     def credentials = config.credentials ?: 'docker-hub-credentials'
@@ -14,6 +14,18 @@ def call(Map config = [:]) {
             echo "\$DOCKER_PASSWORD" | docker login -u "\$DOCKER_USERNAME" --password-stdin
             docker push ${imageName}:${imageTag}
             docker push ${imageName}:latest
+        """
+    }
+}*/
+def call(String project, String imgTag) {
+    withCredentials([usernamePassword(
+        credentialsId: 'dockerHubCred',
+        passwordVariable: 'dockerHubPass',
+        usernameVariable: 'dockerHubUser'
+    )]) {
+        sh """
+            echo \$dockerHubPass | docker login -u \$dockerHubUser --password-stdin
+            docker push \$dockerHubUser/${project}:${imgTag}
         """
     }
 }
